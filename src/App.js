229c648b91567e3b4e62d918ed78205ec2c59856
Friscxion <erwann.champion@outlook.fr>
2022-12-12 14:07:14 +0100

    ajout du programme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {Button, Card, Form} from "react-bootstrap";
 
 function App() {
+  let [input,setInput]=useState("");
+  let [output,setOutput]=useState("");
+
+  const validate = () =>{
+      setOutput(fizz_buzzator(input))
+  }
+  const fizz_buzzator = (numb) =>{
+      numb=parseInt(numb);
+      if(!numb&&numb!==0)return "Pas de valeur entrée";
+      return numb%3===0&&numb%5===0?"Fizz Buzz":numb%3===0?"Fizz":numb%5===0?"Buzz":numb;
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Form.Group className="mb-3">
+                    <Form.Label>Fizz Buzzator</Form.Label>
+                    <Form.Control onChange={({target})=>setInput(target.value)} value={input} type="number" placeholder="Entrez un nombre" />
+                    <Button className={"mt-3 mb-3"} variant="outline-primary" onClick={validate}>Primary</Button>
+                    <Form.Text className="text-muted d-flex">
+                        {output}
+                    </Form.Text>
+                </Form.Group>
+            </Card.Body>
+        </Card>
     </div>
   );
 }
